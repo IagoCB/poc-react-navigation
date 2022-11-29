a74f7b55e08752066d4b781418bb9871ae4ea6de
Iago Luiz <iago.barbosa@nouvenn.com>
2022-11-29 09:57:40 -0300

    :construction: Create Drawer navigation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import routes from './src/routes';
 import {name as appName} from './app.json';
